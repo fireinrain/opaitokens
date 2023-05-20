9a0477ac68b5a615fe068a122bc37cb24d2a4c01
sunrise <575563079@qq.com>
2023-05-20 17:33:03 +0800

    update testcase

--- README.md
+++ README.md
@@ -10,10 +10,15 @@ password := "xxxxxxx"
 tokens := NewOpaiTokens(email, password)
 token := tokens.FetchToken()
 fmt.Printf("token info: %v\n", token)
+accessToken := token.OpenaiToken.AccessToken
+// use the access token
+fmt.Println("i am using access token: %v\n", accessToken)
 
 token = tokens.RefreshToken()
 fmt.Println("token info again: %v\n", token)
-	
+accessToken = token.RefreshedToken.AccessToken
+//use the refresh token
+fmt.Println("i am using refresh token: %",accessToken)
 
 
 ```
--- opaitokens_test.go
+++ opaitokens_test.go
@@ -12,8 +12,14 @@ func TestUserCase(t *testing.T) {
 	tokens := NewOpaiTokens(email, password)
 	token := tokens.FetchToken()
 	fmt.Printf("token info: %v\n", token)
+	accessToken := token.OpenaiToken.AccessToken
+	// use the access token
+	fmt.Println("i am using access token: %v\n", accessToken)
 
 	token = tokens.RefreshToken()
 	fmt.Println("token info again: %v\n", token)
+	accessToken = token.RefreshedToken.AccessToken
+	//use the refresh token
+	fmt.Println("i am using refresh token: %", accessToken)
 
 }
